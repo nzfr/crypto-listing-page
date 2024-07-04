53fe523e63ad50c538778fbf01581cf5da59d8a3
amir.nazarifar <amir.nazarifar@karnameh.ir>
2024-07-04 04:25:41 -0400

    Update ESLint and Prettier configurations, and dependencies
    This commit removes the ESLint configuration file (.eslintrc.cjs) and introduces a new one (.eslintrc.json), along with a new Prettier configuration file (.prettierrc). It also updates the content of the yarn.lock file to reflect changes in dependencies.

---
+++ eslint.config.mjs
@@ -0,0 +1,60 @@
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...fixupConfigRules(compat.extends(
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+)), {
+    plugins: {
+        react: fixupPluginRules(react),
+        "react-hooks": fixupPluginRules(reactHooks),
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+        prettier,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    settings: {
+        "import/resolver": {
+            typescript: {},
+        },
+
+        react: {
+            version: "detect",
+        },
+    },
+
+    rules: {
+        "react/no-unescaped-entities": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "react/react-in-jsx-scope": "off",
+    },
+}];
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 function App() {
-
-    return (
-        <div className='bg-red-400 w-full h-32'>
-            <span className='font-bold text-black'>
-                this is a tailwind test
-            </span>
-        </div>
-    )
+  return (
+    <div className='bg-red-400 w-full h-32'>
+      <span className='font-bold text-black'>this is a tailwind test</span>
+    </div>
+  )
 }
 
 export default App
